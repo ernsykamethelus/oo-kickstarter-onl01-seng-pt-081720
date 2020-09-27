76daffe4c160fa8ae6a75ffeb5ce133a593c9adc
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 02:58:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
+
 class Backer
-  attr_accessor :name 
-  
+  attr_accessor :name, :backed_projects
+
   def initialize(name)
     @name = name
-    @back_project = []
+    @backed_projects = []
   end
-  
+
   def back_project(project)
-    @back_project << project
+    @backed_projects << project
     project.backers << self
+
   end
   
 end
